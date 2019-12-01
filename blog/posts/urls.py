824c4a2884811c742dc62439f8072e16c6f40699
koshihikari77 <inadakoro@gmail.com>
2019-12-02 08:00:45 +0900

    replace view with viewset and replace url with router

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,12 +1,11 @@
 # posts/urls.py
 from django.urls import path
-
+from rest_framework.routers import SimpleRouter
 from .views import PostList, PostDetail, UserList, UserDetail
 
 
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-]
+router = SimpleRouter()
+router.register('users', UserViewset, base_name='users')
+router.register('', PostViewSet, base_name='posts')
+
+urlpatterns = router.urls
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -5,24 +5,11 @@ from django.contrib.auth import get_user_model
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
 
-# ListCreateAPIView -> read-write endpoint
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-# RetrieveUpdateDestoryAPIView -> ALlows read, update, delete
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-# 追加
-class UserList(generics.ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-# 追加
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewset(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
